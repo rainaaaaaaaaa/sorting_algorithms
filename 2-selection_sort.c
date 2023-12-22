1957768edcb32c0b7a51a64a1b3ef2ea23ef99db
Rain <rainymorning0000@gmail.com>
2023-12-22 23:15:01 +0100

    select

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order using the Selection sort algorithm
+ * @array: array
+ * @size: size
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t x, y, index;
+	int temp;
+
+	if (!array)
+		return;
+
+	for (x = 0; x < (size - 1); x++)
+	{
+		index = x;
+		for (y = x + 1; y < size; y++)
+			if (array[index] > array[y])
+				index = y;
+
+		if (x != index)
+		{
+			temp = array[x];
+			array[x] = array[index];
+			array[index] = temp;
+			print_array(array, size);
+		}
+	}
+}
